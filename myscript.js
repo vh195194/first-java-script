c9e4d73cb72afc99fb0b4a91c78b05bd58f08385
vh195194 <45491498+vh195194@users.noreply.github.com>
2018-12-18 09:11:37 -0700

    Added code

---
+++ myscript.js
@@ -0,0 +1,26 @@
+//this function changes the text in the demo
+paragraph
+
+Function myfunction {} {
+    document.getElementById{'demo}.innerHTML
+    "Paragraph Changed";
+} 
+
+//Defining variables
+
+//var x = 5;
+//var y = 6;
+//var z = x + y;
+    
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+    
+//This function chnges the text in the math paragrapgh
+    
+function mathFunction {} {
+    document.getElementById{'math'}.innerHTML=
+    z;
+    
+}
