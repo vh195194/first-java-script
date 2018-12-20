e92e9da4482aa4ca16ce2b0f8cbf75bdf5ac5ab1
vh195194 <45491498+vh195194@users.noreply.github.com>
2018-12-20 09:11:22 -0700

    fixed everything

--- myscript.js
+++ myscript.js
@@ -1,26 +1,18 @@
-//this function changes the text in the demo
-paragraph
+//this function changes the text in the demo paragraph
 
-Function myfunction {} {
-    document.getElementById{'demo}.innerHTML
-    "Paragraph Changed";
+function myFunction() {
+    document.getElementById('demo').innerHTML = "Paragraph Changed";
 } 
 
 //Defining variables
 
-//var x = 5;
-//var y = 6;
-//var z = x + y;
-    
-var x, y, z;
-x = 5;
-y = 6;
-z = x + y;
+var x = 5;
+var y = 6;
+var z = x + y;
     
 //This function chnges the text in the math paragrapgh
     
-function mathFunction {} {
-    document.getElementById{'math'}.innerHTML=
-    z;
+function mathFunction() {
+    document.getElementById('math').innerHTML = z;
     
 }
